af0a2f0b0b75443379da8b8bac4556131d5d85a5
namhihi237 <yentth237@gmail.com>
2021-11-08 16:22:37 +0700

    add get address from lat lng here utils

--- src/utils/here.js
+++ src/utils/here.js
@@ -20,13 +20,23 @@ class HereUtils {
     return encodeURIComponent(address).replace(/%20/g, "+");
   }
 
-
+  //  get lat lng from address
   async getGeoLocation(address) {
     console.log(this.convertToURLParameter(address));
     let res = await this.geocoder.get(`geocode?apiKey=${HERE_API_KEY}&q=${this.convertToURLParameter(address)}`);
 
     if (res.data.items.length > 0) {
       return res.data.items.slice(-1)[0].position;
+    } else {
+      return null;
+    }
+  }
+
+  // get address from lat lng
+  async getAddress(lat, lng) {
+    let res = await this.geocoder.get(`revgeocode?apiKey=${HERE_API_KEY}&at=${lat},${lng}&lang=vi`);
+    if (res.data.items.length > 0) {
+      return res.data.items.slice(-1)[0].address.label;
     } else {
       return null;
     }
