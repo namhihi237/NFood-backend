de6f06d61ff2738cdbaa6ad5cf44e68068b28cca
namhihi237 <yentth237@gmail.com>
2021-11-12 23:44:03 +0700

    update dployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,10 +17,10 @@ module.exports = {
 
   deploy: {
     production: {
-      user: 'poppy237', // user để ssh
-      host: '20.37.38.82', // IP của server này (theo sơ đồ)
+      user: 'doan', // user để ssh
+      host: '13.76.139.61', // IP của server này (theo sơ đồ)
       ref: 'origin/main', // branch để pull source
-      repo: 'git@github.com:namhihi237/Food-delivery-backend.git', // repo của project
+      repo: 'git@github.com:namhihi237/NFood-backend.git', // repo của project
       path: '/home/poppy237/food', // sẽ deploy vào thư mục này
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js --env production' // cmd để deploy
     },
