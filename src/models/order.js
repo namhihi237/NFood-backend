9d38d9b13086663a491b124b3b469a978a506549
namhihi237 <yentth237@gmail.com>
2021-11-16 22:05:27 +0700

    create order model

---
+++ src/models/order.js
@@ -0,0 +1,98 @@
+import mongoose from 'mongoose';
+
+const OrderSchema = new mongoose.Schema(
+  {
+    ownerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Buyer',
+      required: true,
+    },
+    vendorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Vendor',
+      required: true,
+    },
+    shipperId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Shipper',
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    invoiceNumber: {
+      type: String,
+      required: true,
+    },
+    promoCode: {
+      type: String,
+    },
+    discount: {
+      type: Number
+    },
+    shipping: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    paymentStatus: {
+      type: String,
+      enum: ['Pending', 'Paid', 'Cancelled'],
+      default: 'Pending',
+    },
+    orderStatus: {
+      type: String,
+      enum: ['Pending', 'Processing', 'Shipped', 'Delivered', 'Cancelled'],
+      default: 'Pending',
+    },
+    deliveryDate: {
+      type: Date,
+    },
+    orderItems: {
+      type: [
+        {
+          itemId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Item',
+            required: true,
+          },
+          name: {
+            type: String,
+          },
+          buyerId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Buyer',
+            required: true,
+          },
+          quantity: {
+            type: Number,
+            required: true,
+          },
+          price: {
+            type: Number,
+            required: true,
+          },
+          note: {
+            type: String,
+          }
+        },
+      ],
+    }
+
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('order', OrderSchema, 'order');
