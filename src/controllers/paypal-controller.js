3ff503d07d838e4a352bdb6caa48c0ddee2ffd23
namhihi237 <yentth237@gmail.com>
2022-01-22 13:51:30 +0700

    fix create order

--- src/controllers/paypal-controller.js
+++ src/controllers/paypal-controller.js
@@ -213,7 +213,7 @@ class PayPalController {
             estimatedDeliveryTime,
             orderItems,
             paymentMethod: 'CRE',
-            paymentMethod: 'Paid',
+            paymentStatus: 'Paid',
             location: {
               type: 'Point',
               coordinates: [vendor.location.coordinates[0], vendor.location.coordinates[1]]
